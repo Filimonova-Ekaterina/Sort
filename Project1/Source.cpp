343f77035c40aebd19f23b5f81e55413e30b01ac
Ekaterina <161236037+Filimonova-Ekaterina@users.noreply.github.com>
2024-09-09 21:27:37 +0300

    Update Source.cpp

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -3,22 +3,23 @@
 #include <cstdlib>
 #include <ctime>
 using namespace std;
-const int encoding_identifier = 1251; 
-const int array_size = 25; 
+const int encoding_identifier = 1251;
+const int array_size = 5;
 
-void swap(int& element1, int& element2) 
+void swap(int& element1, int& element2)
 {
 	int tmp = element1;
 	element1 = element2;
 	element2 = tmp;
 }
 
-void insertion_sort(int arr[]) 
+void insertion_sort(int arr[])
 {
 	for (int counter = 1; counter < array_size; counter++)
 		for (int counter1 = counter; counter1 > 0; counter1--)
 		{
 			if (arr[counter1] < arr[counter1 - 1]) swap(arr[counter1], arr[counter1 - 1]);
+			else break;
 		}
 }
 
@@ -27,24 +28,21 @@ void selection_sort(int arr[])
 	for (int counter = 0; counter < array_size; counter++)
 	{
 		int min = counter;
-		for (int counter1 = counter+1; counter1 < array_size; counter1++)
+		for (int counter1 = counter + 1; counter1 < array_size; counter1++)
 		{
 			if (arr[counter1] < arr[min])min = counter1;
 		}
 		swap(arr[counter], arr[min]);
 	}
 }
-void random_sort(int arr[]) 
+void random_sort(int arr[])
 {
-	int sum = 0;
-	for (int counter = 0; counter < array_size-1; counter++) { if (arr[counter] <= arr[counter + 1])sum++; }
-	while (sum < array_size)
+	int sum=0;
+	for (int counter = 0; counter < array_size - 1; counter++) { if (arr[counter] <= arr[counter + 1]) sum++; }
+	while (sum < array_size-1)
 	{
 		srand(time(0));
-		for (int counter = 0; counter < array_size; counter++)
-		{
-			swap(arr[counter], arr[rand() % array_size]);
-		}
+		swap(arr[rand() % array_size], arr[rand() % array_size]);
 		sum = 0;
 		for (int counter = 0; counter < array_size - 1; counter++) { if (arr[counter] <= arr[counter + 1])sum++; }
 	}
@@ -54,7 +52,7 @@ int main()
 {
 	SetConsoleCP(encoding_identifier);
 	SetConsoleOutputCP(encoding_identifier);
-	int arr[array_size],arr1[array_size], arr2[array_size];
+	int arr[array_size], arr1[array_size], arr2[array_size];
 	cout << "Initial array:\n";
 	srand(time(0));
 	for (int counter = 0; counter < array_size; counter++)
@@ -72,22 +70,22 @@ int main()
 	for (int counter = 0; counter < array_size; counter++)
 	{
 		arr2[counter] = rand() % array_size;
-		cout << arr1[counter] << ' ';
+		cout << arr2[counter] << ' ';
 	}
 	insertion_sort(arr);
-	selection_sort(arr1);
-	random_sort(arr2);
 	cout << "\nSort array:\n";
 	for (int counter = 0; counter < array_size; counter++)
 	{
 		cout << arr[counter] << ' ';
 	}
 	cout << "\n";
+	selection_sort(arr1);
 	for (int counter = 0; counter < array_size; counter++)
 	{
 		cout << arr1[counter] << ' ';
 	}
 	cout << "\n";
+	random_sort(arr2);
 	for (int counter = 0; counter < array_size; counter++)
 	{
 		cout << arr2[counter] << ' ';
